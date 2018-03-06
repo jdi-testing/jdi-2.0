7b5a80a0b067cb1e73852de76ca13adc120e0f22
Supreme85 <romanyister@gmail.com>
2018-03-07 01:52:16 +0300

    fix droplist issue

--- Frameworks/JDIWeb/jdi-uitest-web-base/src/main/java/com/epam/jdi/uitests/web/selenium/elements/complex/DropList.java
+++ Frameworks/JDIWeb/jdi-uitest-web-base/src/main/java/com/epam/jdi/uitests/web/selenium/elements/complex/DropList.java
@@ -5,6 +5,7 @@ package com.epam.jdi.uitests.web.selenium.elements.complex;
  * Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
  */
 
+import com.epam.jdi.uitests.core.annotations.JDIAction;
 import com.epam.jdi.uitests.core.interfaces.ISetup;
 import com.epam.jdi.uitests.core.interfaces.complex.IDropList;
 import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JDropList;
@@ -12,7 +13,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
+import static com.epam.jdi.tools.LinqUtils.toIntegerArray;
+import static com.epam.jdi.tools.LinqUtils.toStringArray;
+import static com.epam.jdi.tools.LinqUtils.where;
 import static com.epam.jdi.uitests.web.selenium.elements.base.LinkedSetup.setupDropList;
 import static com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.WebAnnotationsUtil.findByToBy;
 import static com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.FillFromAnnotationRules.fieldHasAnnotation;
@@ -45,4 +50,49 @@ public class DropList<TEnum extends Enum> extends Dropdown<TEnum>
             : el;
         return super.isSelected(element);
     }
+    @JDIAction
+    public void check(String... names) {
+        expand();
+        List<String> deselected = where(names, name -> !isSelected(name));
+        select(toStringArray(deselected));
+    }
+
+    @JDIAction
+    public void check(Integer... indexes) {
+        expand();
+        List<Integer> deselected = where(indexes, index -> !isSelected(index));
+        select(toIntegerArray(deselected));
+    }
+
+    @JDIAction @Override
+    public void uncheck(String... names) {
+        expand();
+        List<String> selected = where(names, this::isSelected);
+        select(toStringArray(selected));
+    }
+
+    @JDIAction @Override
+    public void uncheck(Integer... indexes) {
+        expand();
+        List<Integer> selected = where(indexes, this::isSelected);
+        select(toIntegerArray(selected));
+    }
+
+    /**
+     * @return Get names of checked options
+     */
+    @JDIAction @Override
+    public List<String> areSelected() {
+        expand();
+        return where((List<String>) getNames(), this::isSelected);
+    }
+    /**
+     * @return Get names of unchecked options
+     */
+    @JDIAction @Override
+    public List<String> areDeselected() {
+        expand();
+        return where((List<String>) getNames(), name -> !isSelected(name));
+    }
+
 }
